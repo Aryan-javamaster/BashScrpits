8a3d6af0bddd55b5a7bf96adebd8098b5d610544
Aryan <119639573+Aryan-javamaster@users.noreply.github.com>
2023-03-20 15:48:34 -0500

    Update docker-portainer-install.sh

--- docker-portainer-install.sh
+++ docker-portainer-install.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-echo "do you want to install docker(y/n)"
-
+# Prompt the user whether they want to install Docker
+echo "do you want to install docker (y/n)"
 read result
 
-#docker install
-
-
+# Function to install Docker and Docker Compose
 docker_install () {
 	echo "updating repo's"
 	sleep 2
+
+	# Update the repository
 	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 	sudo apt update
 	echo "done"
+
+	# Install required dependencies
 	echo "installing required dependency"
 	sleep 2
 	sudo apt install ca-certificates curl gnupg lsb-release -y
 	echo "done"
-	echo "getting offical gpg key"
+
+	# Get official GPG key
+	echo "getting official gpg key"
 	sleep 2
 	sudo mkdir -m 0755 -p /etc/apt/keyrings
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 	echo "done"
+
+	# Set up repository
 	echo "setting up repo's"
 	sleep 2
 	echo \
@@ -32,32 +38,37 @@ docker_install () {
 	sudo apt-get update
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 	echo "done.."
+	
+	# Test the installation
 	echo "testing"
 	sudo docker run hello-world
-	echo "Done docker is now installed thanks for using this script -- ARYAN SINGH"
+	echo "Docker is now installed thanks for using this script -- ARYAN SINGH"
 }
 
+# Function to install and run Portainer
 portainer_install () {
 	sudo docker volume create portainer_data
 	sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 }
 
+# If the user wants to install Docker
 if [[ $result == "y" ]]; then
 	echo "Downloading......docker"
 	docker_install
-	echo "do you want to install poratiner: Portianer is a GUI to manage all your conatiner (y/n)"
+	
+	# Prompt the user whether they want to install Portainer
+	echo "do you want to install portainer: Portainer is a GUI to manage all your containers (y/n)"
 	read portainer
+	
+	# If the user wants to install Portainer
 	if [[ $portainer == "y" ]]; then 
 		portainer_install
 		ip=$(hostname -I)
-		echo "portainer is now installed and running you can access the GUI at https://$ip:9443"
-		echo "thank you again :)"
+		echo "Portainer is now installed and running. You can access the GUI at https://$ip:9443"
+		echo "Thank you again :)"
 	else 
 		exit
 	fi
 else 
 	exit
 fi 
-
-
-
