9c8184383f21c33caaefc1c4437c9cbf46dcc997
Aryan <119639573+Aryan-javamaster@users.noreply.github.com>
2023-03-20 15:07:24 -0500

    Update docker-portainer-install.sh

--- docker-portainer-install.sh
+++ docker-portainer-install.sh
@@ -37,8 +37,8 @@ docker_install () {
 }
 
 portainer_install () {
-	docker volume create portainer_data
-	docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+	sudo docker volume create portainer_data
+	sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 }
 
 if [[ $result == "y" ]]; then
